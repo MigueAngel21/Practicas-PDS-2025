f6de18821650beebc26cedabb81898fe052b2258
Miguel Marin <miguelangel.marinv@um.es>
2025-03-12 14:00:55 +0100

    Interfaz Terminada
    Ya estan todas las ventanas terminadas, solo falta implementar el
    dominio y la persistencia.

--- pds/src/main/java/umu/pds/interfaz/Pregunta11.java
+++ pds/src/main/java/umu/pds/interfaz/Pregunta11.java
@@ -1,30 +1,26 @@
 package umu.pds.interfaz;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 
-public class Pregunta1 extends JFrame {
+public class Pregunta11 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -36,7 +32,7 @@ public class Pregunta1 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Pregunta1 frame = new Pregunta1();
+					Pregunta11 frame = new Pregunta11();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -48,8 +44,7 @@ public class Pregunta1 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Pregunta1() {
-		
+	public Pregunta11() {
 		try {
 			UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
 		} catch (ClassNotFoundException e) {
@@ -68,7 +63,7 @@ public class Pregunta1 extends JFrame {
 		
 		setBounds(420, 160, 732, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Pregunta 1");
+		setTitle("Pregunta 11");
 		setResizable(false);
 		//cambiar icono de la ventana
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src/main/java/umu/pds/resources/musculitos.png"));
@@ -103,12 +98,12 @@ public class Pregunta1 extends JFrame {
         panelPrincipal.setBorder(BorderFactory.createEmptyBorder(15, 20, 15, 20));
 
         // Pregunta en negrita
-        JLabel lblPregunta = new JLabel("Completa la frase de Luis Rubiales : ");
+        JLabel lblPregunta = new JLabel("Responde a la siguiente pregunta : ");
         lblPregunta.setFont(new Font("Arial", Font.BOLD, 16));
         lblPregunta.setForeground(Color.BLACK);
 
         // Frase con hueco para completar
-        JLabel lblFrase = new JLabel("Ella me dijo, eres un crack, y yo le dije, _________?");
+        JLabel lblFrase = new JLabel("¿Cuáles de estos jugadores no ha ganado un balón de oro?");
         lblFrase.setFont(new Font("Arial", Font.PLAIN, 14));
         lblFrase.setForeground(Color.BLACK);
         
@@ -118,9 +113,9 @@ public class Pregunta1 extends JFrame {
         panelOpciones.setBackground(Color.WHITE);
 
         // Botones de opción (más pequeños)
-        JButton opcion1 = new JButton("1. un abrazo");
-        JButton opcion2 = new JButton("2. un piquito");
-        JButton opcion3 = new JButton("3. un apreton de manos");
+        JButton opcion1 = new JButton("1. Luka Modric");
+        JButton opcion2 = new JButton("2. Karim Benzema");
+        JButton opcion3 = new JButton("3. Dani Carvajal");
 
         // Ajustar tamaño de los botones
         Dimension botonSize = new Dimension(100, 30);
@@ -173,8 +168,8 @@ public class Pregunta1 extends JFrame {
         	// ocultar la ventana de registro
         	this.setVisible(false);
 			// abrir la ventana principal
-        	Pregunta2 p2 = new Pregunta2();
-			p2.setVisible(true);
+        	Pregunta12 p12 = new Pregunta12();
+			p12.setVisible(true);
 		});
 
         // Agregar componentes al panel principal
