fc555c4c864330fcc0af7f68f991eddfc487ea4e
Daniel <danielantonio.martinezs@um.es>
2025-04-03 20:08:05 +0200

    implementación del progreso y estadisticas

--- pds/src/main/java/umu/pds/dominio/Usuario.java
+++ pds/src/main/java/umu/pds/dominio/Usuario.java
@@ -10,11 +10,48 @@ public class Usuario {
 	private int edad;
 	private Estadistica estadisticas;
 	private List<Curso> cursos;
+	
+	public Usuario(String nombre, String email, String password, int edad) {
+		this.nombre = nombre;
+		this.email = email;
+		this.password = password;
+		this.edad = edad;
+		this.estadisticas = new Estadistica();
+	}
 
+	public void updateEstadisticas(Estadistica estadisticas) {
+		this.estadisticas = estadisticas;
+	}
+	
 	public boolean comprobarContrasena(String contrasena) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public Estadistica getEstadisticas() {
+		return estadisticas;
+	}
+
+	public List<Curso> getCursos() {
+		return cursos;
+	}
 	
+
 
 }
