2656330fe6fddf4a82598e4eb3299449ec7c07f7
Daniel <danielantonio.martinezs@um.es>
2025-05-12 15:53:02 +0200

    Arreglar Junit5 para github actions

--- pds/src/test/java/umu/pds/controlador/ControladorTestBD.java
+++ pds/src/test/java/umu/pds/controlador/ControladorTestBD.java
@@ -1,12 +1,7 @@
 package umu.pds.controlador;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
